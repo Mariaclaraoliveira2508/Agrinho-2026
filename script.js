de1640e63099910f4be5753cd14aa38941cc8fc8
Mariaclaraoliveira2508 <souza.oliveira.maria2508@escola.pr.gov.br>
2026-05-25 13:37:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function mostrarMensagem() {
 
+  alert(
+    "🌱 O futuro sustentável começa com atitudes conscientes no campo e na cidade."
+  );
+}
+
+function mostrarAlerta() {
+
+  alert(
+    "💚 Obrigado por apoiar um agro forte e sustentável!"
+  );
+}
